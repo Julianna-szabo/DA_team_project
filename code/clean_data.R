b944a0059ed6913be3e93affe246dbf55b3762f7
Maeva Braeckevelt <Braeckevelt_maeva@student.ceu.edu>
2020-10-17 14:31:55 +0200

    Combined calculation and analysis script

--- code/clean_data.R
+++ code/clean_data.R
@@ -5,8 +5,9 @@ rm(list=ls())
 #install.packages("geosphere")
 library(tidyverse)
 library(geosphere)
+library(moments)
 
-dp <- read_csv("data/raw/Data_Pizza.csv")
+dp <- read_csv("data/raw/dp.csv")
 ceu = c(47.501348, 19.049375)
 dp["dist_ceu"] = distm(data.matrix(dp[6:7]), ceu, fun = distHaversine)
 distm(ceu, c(47.500386, 19.049434), fun=distHaversine)
@@ -24,3 +25,64 @@ get_interval <- function(open,close){
 }
 
 dp$open_mins=mapply(get_interval, dp$open, dp$close)
+
+
+## data Analysis
+summarise(dp,
+          n= n(),
+          mean = mean(x = price_marg),
+          median = median(x = price_marg),
+          min= min(price_marg),
+          max = max(price_marg),
+          sd = sd(price_marg),
+          skew = skewness(price_marg))
+
+summarise(dp,
+          n= n(),
+          mean = mean(x = price_bev),
+          median = median(x = price_bev),
+          min= min(price_bev),
+          max = max(price_bev),
+          sd = sd(price_bev),
+          skew = skewness(price_bev))
+
+
+## Data Visulization Part 1
+dp %>% 
+  ggplot(aes(x=price_marg))+
+  geom_histogram(binwidth = 200, fill= "coral2", col= "black")+
+  theme_bw()+
+  labs(x = 'Price of Margarita Pizza', y= 'Number of Pizzas', title = "Distribution of Price for Pizza")
+
+dp %>% 
+  ggplot(aes(x=price_bev))+
+  geom_histogram(binwidth = 50, fill= "light blue", col= "black")+
+  theme_bw()+
+  labs(x = 'Price of 0.5L beverage', y= 'Number of Pizzas', title = "Distribution of Price for Beverage")
+
+
+## Data Visualization Part 2 - to be worked on
+dp %>%
+  filter(category == "On-site") %>% 
+  ggplot(aes(x=price_marg))+
+  geom_histogram(binwidth = 200, fill= "peachpuff2", col= "black")+
+  theme_bw()+
+  labs(x = 'Price of Margarita Pizza', y= 'Number of Pizzas', title = "Distribution of Price for On-site")
+
+dp %>%
+  filter(category == "Delivery") %>% 
+  ggplot(aes(x=price_marg))+
+  geom_histogram(binwidth = 200, fill= "plum2", col= "black")+
+  theme_bw()+
+  labs(x = 'Price of Margarita Pizza', y= 'Number of Pizzas', title = "Distribution of Price for Delivery")
+
+#T -test
+
+# Hypothesis
+# Null mean(on-site)=mean(delivery)
+# Alternative mean(on-site) not = mean(delivery)
+
+t.test(price_marg,
+       alternative = "two.sided", mu = 0, var.equal = TRUE, 
+)
+
