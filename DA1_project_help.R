7b04e40b9719d7bb26ac774cbd58fda0a6ad86b6
Terez Julianna Szabo <szabo_terez@student.ceu.edu>
2020-10-17 14:01:40 +0200

    added the Analysis section to the R script

--- DA1_project_help.R
+++ DA1_project_help.R
@@ -1,11 +1,16 @@
 rm(list=ls())
-data_storage <- 'C:/USERS/mbrae/OneDrive/Bureau/CEU/DA1/DA_team_project/data'
-data_Pizza <- read.csv(paste0(data_storage,"/raw/data_Pizza.csv"))
+library(tidyverse)
+library(moments)
+#install.packages("moments")
+
+data_storage <- '/Users/Terez/OneDrive - Central European University/Data_Analysis_01/DA_team_project/data'
+data_Pizza <- read_csv(paste0(data_storage,"/raw/data_Pizza.csv"))
 
 ## Data cleaning
 
 data_Pizza = data_Pizza[-1,]
 row.names(data_Pizza) <- df$id
+data_Pizza$id <- NULL
 
 str(data_Pizza)
 data_Pizza$price_marg <- as.numeric(data_Pizza$price_marg)
@@ -17,11 +22,7 @@ data_Pizza$no_ratings <- as.numeric(data_Pizza$no_ratings)
 data_Pizza$price_bev <- as.numeric(data_Pizza$price_bev)
 data_Pizza$price_del <- as.numeric(data_Pizza$price_del)
 
-
-
 ## data Analysis
-library(tidyverse)
-library(moments)
 summarise(data_Pizza,
           n= n(),
           mean = mean(x = price_marg),
@@ -39,4 +40,44 @@ summarise(data_Pizza,
           max = max(price_bev),
           sd = sd(price_bev),
           skew = skewness(price_bev))
+
+
+## Data Visulization Part 1
+data_Pizza %>% 
+ggplot(aes(x=price_marg))+
+    geom_histogram(binwidth = 200, fill= "coral2", col= "black")+
+    theme_bw()+
+    labs(x = 'Price of Margarita Pizza', y= 'Number of Pizzas', title = "Distribution of Price for Pizza")
+
+data_Pizza %>% 
+  ggplot(aes(x=price_bev))+
+  geom_histogram(binwidth = 50, fill= "light blue", col= "black")+
+  theme_bw()+
+  labs(x = 'Price of 0.5L beverage', y= 'Number of Pizzas', title = "Distribution of Price for Beverage")
+ 
+
+## Data Visualization Part 2 - to be worked on
+data_Pizza %>%
+filter(category == "On-site") %>% 
+  ggplot(aes(x=price_marg))+
+  geom_histogram(binwidth = 200, fill= "peachpuff2", col= "black")+
+  theme_bw()+
+  labs(x = 'Price of Margarita Pizza', y= 'Number of Pizzas', title = "Distribution of Price for On-site")
+
+data_Pizza %>%
+filter(category == "Delivery") %>% 
+  ggplot(aes(x=price_marg))+
+  geom_histogram(binwidth = 200, fill= "plum2", col= "black")+
+  theme_bw()+
+  labs(x = 'Price of Margarita Pizza', y= 'Number of Pizzas', title = "Distribution of Price for Delivery")
+
+#T -test
+
+# Hypothesis
+  # Null mean(on-site)=mean(delivery)
+  # Alternative mean(on-site) not = mean(delivery)
+
+t.test(price_marg,
+       alternative = "two.sided", mu = 0, var.equal = TRUE, 
+       )
 
