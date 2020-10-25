b4950c7d4264956295d5913bb4f863e3ff652e58
Maeva Braeckevelt <Braeckevelt_maeva@student.ceu.edu>
2020-10-25 12:27:08 +0100

    Add Proper commenting and spacing to Clean_data

--- code/clean_data.R
+++ code/clean_data.R
@@ -13,6 +13,7 @@ rm(list=ls())
 
 ## Install and import packages
 #install.packages("geosphere")
+#install.packages("pander")
 library(tidyverse)
 library(geosphere)
 library(moments)
@@ -73,7 +74,8 @@ dp %>%
   ggplot(aes(x=price_marg))+
   geom_histogram(binwidth = 200, fill= "coral2", col= "black")+
   theme_bw()+
-  labs(x = 'Price of Margarita Pizza (HUF)', y= 'Number of Pizzas', title = "Distribution of Price for Pizza")
+  labs(x = 'Price of Margarita Pizza (HUF)', y= 'Number of Pizzas', 
+       title = "Distribution of Price for Pizza")
 
 # Histogram : descriptive graph of price distribution of the beverage by catgegories
 dp %>% 
@@ -81,7 +83,8 @@ dp %>%
   geom_histogram(binwidth = 50, col= "black")+
   theme_bw()+
   scale_fill_brewer()+
-  labs(x = 'Price of 0.5L beverage (HUF)', y= 'Number of Pizzas', title = "Distribution of Price for Beverage by category")
+  labs(x = 'Price of 0.5L beverage (HUF)', y= 'Number of Pizzas',
+       title = "Distribution of Price for Beverage by category")
 
 
 ## Creating a factor variable
@@ -91,8 +94,17 @@ dp$category_f <- factor(dp$category)
 #  Density :  Comparaison of price distribution of the Margarita On-site and Delivery
 ggplot(data = dp, aes(x = price_marg, fill = category_f, alpha = 0.2))+
   geom_density(col = "black")+
-  labs(x='Price of Margarita Pizza (HUF)', y='Number of Pizzas', title = "Distribution of Price for On-site and Delivery", fill= 'category')
+  labs(x='Price of Margarita Pizza (HUF)', y='Number of Pizzas', 
+       title = "Distribution of Price for On-site and Delivery", fill= 'category')
 
+# Scatter plot : relationship between delivery price and distance from CEU Campus
+dp_del <- dp[dp$price_del!='N/A',] %>% 
+  transform(price_del = as.numeric(price_del))
+corr=round(cor(dp_del$dist_ceu, dp_del$price_del, method = c("pearson")),4)
+ggplot(dp_del, aes(x=dist_ceu, y=price_del)) +
+  geom_point(size=3, color='orange') +
+  labs(x='Distance from CEU', y='Delivery price', title = expr(paste
+    ("Relationship between delivery price and distance from CEU Campus, ", rho, "=",!!corr)))
 
 ## HYPOTHESIS TESTING
 
